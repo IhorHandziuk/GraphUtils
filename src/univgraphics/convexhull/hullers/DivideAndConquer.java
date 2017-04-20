6ba51b281862f2a8876e9606e6e950a133bec3bc
igorg <igor.gandziuk@gmail.com>
2017-04-20 12:50:36 +0300

    skeleton for divide and conquer method

--- src/univgraphics/convexhull/hullers/DivideAndConquer.java
+++ src/univgraphics/convexhull/hullers/DivideAndConquer.java
@@ -4,6 +4,7 @@ import univgraphics.common.*;
 import univgraphics.common.primitives.Node;
 import univgraphics.common.primitives.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +18,31 @@ public class DivideAndConquer extends Huller {
 
     @Override
     public List<Point> getRegion() {
-        return (new Jarvis(graph)).getRegion();
+        if (graph.size() < 3) {
+            return null;
+        }
+        return divideAndConquer(graph);
+    }
+
+    private static List<Point> divideAndConquer(List<Node> points) {
+        if (points.size() < 5) {
+            return (new Jarvis(points)).getRegion();
+        }
+        List<Node> firstHalf = new ArrayList<>();
+        List<Node> secondHalf = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            if (i < points.size() / 2) {
+                firstHalf.add(points.get(i));
+            } else {
+                secondHalf.add(points.get(i));
+            }
+        }
+        List<Point> firstHull = divideAndConquer(firstHalf);
+        List<Point> secondHull = divideAndConquer(secondHalf);
+        return uniteConvexHulls(firstHull, secondHull);
+    }
+
+    private static List<Point> uniteConvexHulls(List<Point> first, List<Point> second) {
+        return null;
     }
 }
