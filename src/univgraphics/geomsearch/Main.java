abcb90f48dbb35334a1a39681870fa8eb1f9012a
igorg <igor.gandziuk@gmail.com>
2017-04-22 19:33:11 +0300

    front-end changes

--- src/univgraphics/geomsearch/Main.java
+++ src/univgraphics/geomsearch/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
-        primaryStage.setTitle("Point localization");
+        primaryStage.setTitle("Geometrical search");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
--- src/univgraphics/proximity/Main.java
+++ src/univgraphics/proximity/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
-        primaryStage.setTitle("Point localization");
+        primaryStage.setTitle("Proximity");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
